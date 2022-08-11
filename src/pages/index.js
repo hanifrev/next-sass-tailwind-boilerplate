6ce047f8c7fb38070af3146b490376dd279f57eb
hanifrev <hanifrev@gmail.com>
2022-08-11 11:05:34 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div id="home">
+      <div className="font-bold bg-blue-400 test">Blackwater Park</div>
+    </div>
+  );
+};
+
+export default index;
